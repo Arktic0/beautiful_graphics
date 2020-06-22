9aec0fb88531977b8897296cd3634c13f8ca0bca
Max Taldykin <jorpic@gmail.com>
2020-06-22 20:23:28 +0300

    Simplify datetimes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from collections import defaultdict
 
 filename = str(input("путь:"))
@@ -6,8 +6,8 @@ fdate = str(input("начальная дата:"))
 ldate = str(input("конечная дата:"))
 
 cdict = defaultdict(list)
-fcdate = datetime.datetime.strptime(fdate, '%Y-%m-%d')
-lcdate = datetime.datetime.strptime(ldate, '%Y-%m-%d')
+fcdate = datetime.strptime(fdate, '%Y-%m-%d').date()
+lcdate = datetime.strptime(ldate, '%Y-%m-%d').date()
 
 fuserid = str(input('1 id:'))
 suserid = str(input('2 id:'))
@@ -18,14 +18,14 @@ with open(filename, 'r') as file:
         ln[1] = ln[1].strip('\n')
         ln[1] += "00"
         try:
-            ctime = datetime.datetime.strptime(ln[1], '%Y-%m-%d %H:%M:%S.%f%z')
+            ctime = datetime.strptime(ln[1], '%Y-%m-%d %H:%M:%S.%f%z')
         except ValueError:
-            ctime = datetime.datetime.strptime(ln[1], '%Y-%m-%d %H:%M:%S%z')
+            ctime = datetime.strptime(ln[1], '%Y-%m-%d %H:%M:%S%z')
         if ln[0] == fuserid:
-            if fcdate.date() <= ctime.date() <= lcdate.date():
+            if fcdate <= ctime.date() <= lcdate:
                 cdict[ln[0]].append(ln[1])
         if ln[0] == suserid:
-            if fcdate.date() <= ctime.date() <= lcdate.date():
+            if fcdate <= ctime.date() <= lcdate:
                 cdict[ln[0]].append(ln[1])
 
 print(cdict)
