c227653fe451bb51f219085bc481bd8a08f6fc53
Max Taldykin <jorpic@gmail.com>
2020-06-26 11:50:51 +0300

    Nested groups: user id, time bucket, state

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ filename = str(input("путь:"))
 fdate = str(input("начальная дата:"))
 ldate = str(input("конечная дата:"))
 
-cdict = defaultdict(list)
 fcdate = datetime.strptime(fdate, '%Y-%m-%d').date()
 lcdate = datetime.strptime(ldate, '%Y-%m-%d').date()
 
@@ -13,22 +12,38 @@ fuserid = str(input('1 id:'))
 suserid = str(input('2 id:'))
 users = [fuserid, suserid]
 
+
+def parse_time(s):
+    s = s.strip(' \n') + "00"
+    try:
+        return datetime.strptime(s, '%Y-%m-%d %H:%M:%S.%f%z')
+    except ValueError:
+        return datetime.strptime(s, '%Y-%m-%d %H:%M:%S%z')
+
+
+def time_group(dt):
+    return dt.strftime('%Y-%m-%d %H')
+
+
+data = {}
 with open(filename, 'r') as file:
     for ln in file:
         ln = ln.split(",")
-        ln[1] = ln[1].strip('\n')
-        ln[1] += "00"
-        try:
-            ctime = datetime.strptime(ln[1], '%Y-%m-%d %H:%M:%S.%f%z')
-        except ValueError:
-            ctime = datetime.strptime(ln[1], '%Y-%m-%d %H:%M:%S%z')
-        if ln[4] in users and fcdate <= ctime.date() <= lcdate:
-            row = {
-                "userId": ln[4],
-                "state": ln[2],
-                "ctime": ctime
-            }
-            cdict[row["userId"]].append(row)
 
-print(len(cdict[fuserid]))
-print(len(cdict[suserid]))
+        ctime = parse_time(ln[1])
+        user_id = ln[4]
+        state = ln[2]
+        state_range = None
+        if len(ln) > 5:
+            state_range = ln[5] # TODO: parse time range
+
+        if user_id in users and fcdate <= ctime.date() <= lcdate:
+            z = data.setdefault(user_id, {})
+            z = z.setdefault(time_group(ctime), {})
+            z = z.setdefault(state, [])
+            z.append({"ctime": ctime, "range": state_range})
+
+
+states = data[fuserid]['2017-03-13 17']
+for s in states.items():
+    print(s)
