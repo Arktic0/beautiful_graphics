2f09456492d82a6a749f3f106f7efc778857291f
Arktic0 <44374959+Arktic0@users.noreply.github.com>
2020-07-28 17:10:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+# Libs
 from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = str(input("путь:"))
-fdate = str(input("начальная дата:"))
-ldate = str(input("конечная дата:"))
+# Input
+filename = str(input("Path:"))
+fdate = str(input("First Date:"))
+ldate = str(input("Last Date:"))
 
-fcdate = datetime.strptime(fdate, '%Y-%m-%d').date()
-lcdate = datetime.strptime(ldate, '%Y-%m-%d').date()
+try:
+    fcdate = datetime.strptime(fdate, '%Y-%m-%d').date()
+    lcdate = datetime.strptime(ldate, '%Y-%m-%d').date()
+except ValueError:
+    print('Date Input Error, Incorrect Date')
+    quit()
 
-fuserid = str(input('1 id:'))
-suserid = str(input('2 id:'))
+fuserid = str(input('1 ID:'))
+suserid = str(input('2 ID:'))
 users = [fuserid, suserid]
 
 
+# Defs
 def parse_time(s):
     s = s.strip(' \n[)"') + "00"
     try:
@@ -22,101 +29,193 @@ def parse_time(s):
         return datetime.strptime(s, '%Y-%m-%d %H:%M:%S%z')
 
 
-def time_group(dt):
+def group_time(dt):
     return dt.strftime('%Y-%m-%d %H')
 
 
-data = {}
-with open(filename, 'r') as file:
-    for ln in file:
-        ln = ln.split(",")
-        ctime = parse_time(ln[1])
-        user_id = ln[4]
-        state = ln[2]
-        if user_id in users and fcdate <= ctime.date() <= lcdate:
-            state_range = None
-            if len(ln) > 6:
-                state_range = parse_time(ln[6]) - parse_time(ln[5])
-            z = data.setdefault(user_id, {})
-            z = z.setdefault(time_group(ctime), {})
-            z.setdefault(state, 0)
-            if state_range:
-                z[state] += state_range.seconds
+def distribute_overflows(du):
 
-rneeddate = {}
-rneed = []
-rneedstate = {}
-i = 0
-pp = 0
-while pp != 2:
-    pp += 1
-    for key, value in data[fuserid].items():
+    ost = {}
+
+    for key, value in du.items():
+        lneed = []
         for smt, need in value.items():
+            if need < 3600:
+                lneed.append(need)
+            if sum(lneed) > 3600:
+                lneed.sort()
+                du[key].update({smt: need + 3600 - sum(lneed)})
+                ost[key] = {smt: sum(lneed) - 3600}
             if need > 3600:
-                rneedstate[smt] = 3600
-                rneed.append(need)
-                key = datetime.strptime(key, '%Y-%m-%d %H')
-                rneeddate[key] = key
-                while need > 3600:
-                    need -= 3600
+                if len(lneed) != 0:
+                    ost[key] = {smt: need - (3600 - sum(lneed))}
+                    du[key].update({smt: 3600 - sum(lneed)})
+                else:
+                    ost[key] = {smt: need - 3600}
+                    du[key].update({smt: 3600})
 
-    for l in rneeddate:
-        groups = np.arange(rneeddate[l], lcdate, timedelta(hours=1)).astype(datetime)
-        try:
-            while rneed[i] > 3600:
-                rneed[i] -= 3600
-                grp_key = time_group(groups[i])
-                data[fuserid][grp_key] = rneedstate
-                this_group = data[fuserid][grp_key]
-                i += 1
-        except IndexError:
-            continue
+    rkey = datetime.strptime(key, '%Y-%m-%d %H')
+    groups = np.arange(rkey, lcdate, timedelta(hours=1)).astype(datetime)
+    j = 0
+    b = 0
 
+    for k in groups:
+        grp_key = group_time(groups[j])
+        j += 1
+        if grp_key not in du:
+            du.update({grp_key: {}})
+        if b != 0:
+            break
+        for ost_key, ost_value in ost.items():
+            if sum(list(du[grp_key].values())) == 3600:
+                break
+            if b != 0:
+                break
+            for ost_state, ost_time in ost_value.items():
+                if b != 0:
+                    break
+                if ost_state in du[grp_key]:
+                    if ost_time <= 3600 - sum(list(du[grp_key].values())):
+                        du[grp_key].update({ost_state: ost_time + du[grp_key][ost_state]})
+                        ost[ost_key].update({ost_state: 0})
+                    else:
+                        i = j
+                        try:
+                            for kk in groups:
+                                if b != 0:
+                                    break
+                                if ost_time > 3600:
+                                    ost_time -= 3600 - sum(list(du[grp_key].values()))
+                                    du[grp_key].update({ost_state: du[grp_key][ost_state] + 3600 - sum(list(du[grp_key].values()))})
+                                    ost[ost_key].update({ost_state: ost_time})
+                                    grp_key = group_time(groups[j])
+                                    i += 1
+                                else:
+                                    du[grp_key].update({ost_state: ost_time})
+                                    ost[ost_key].update({ost_state: 0})
+                        except KeyError:
+                            try:
+                                for kkk in groups:
+                                    if b != 0:
+                                        break
+                                    if ost_time > 3600:
+                                        if grp_key not in du:
+                                            du.update({grp_key: {}})
+                                        ost_time -= 3600 - sum(list(du[grp_key].values()))
+                                        du[grp_key].update({ost_state: 3600 - sum(list(du[grp_key].values()))})
+                                        ost[ost_key].update({ost_state: ost_time})
+                                        grp_key = group_time(groups[i])
+                                        i += 1
+                                    else:
+                                        grp_key = group_time(groups[i])
+                                        if grp_key not in du:
+                                            du.update({grp_key: {}})
+                                        du[grp_key].update({ost_state: ost_time})
+                                        ost[ost_key].update({ost_state: 0})
+                            except IndexError:
+                                b += 1
+                                break
+                else:
+                    if ost_time <= 3600 - sum(list(du[grp_key].values())):
+                        du[grp_key].update({ost_state: ost_time})
+                        ost[ost_key].update({ost_state: 0})
+                    else:
+                        i = j
+                        try:
+                            for kkkk in groups:
+                                if ost_time > 3600:
+                                    if grp_key not in du:
+                                        du.update({grp_key: {}})
+                                    ost_time -= 3600 - sum(list(du[grp_key].values()))
+                                    du[grp_key].update({ost_state: 3600 - sum(list(du[grp_key].values()))})
+                                    ost[ost_key].update({ost_state: ost_time})
+                                    grp_key = group_time(groups[i])
+                                    i += 1
+                                else:
+                                    grp_key = group_time(groups[i])
+                                    if grp_key not in du:
+                                        du.update({grp_key: {}})
+                                    du[grp_key].update({ost_state: ost_time})
+                                    ost[ost_key].update({ost_state: 0})
+                        except IndexError:
+                            b += 1
+                            break
+
+    return du
+
+
+# Parsing
+data = {}
+try:
+    with open(filename, 'r') as file:
+        for ln in file:
+            ln = ln.split(",")
+            ctime = parse_time(ln[1])
+            user_id = ln[4]
+            state = ln[2]
+            if user_id in users and fcdate <= ctime.date() <= lcdate:
+                state_range = None
+                if len(ln) > 6:
+                    state_range = parse_time(ln[6]) - parse_time(ln[5])
+                z = data.setdefault(user_id, {})
+                z = z.setdefault(group_time(ctime), {})
+                z.setdefault(state, 0)
+                if state_range:
+                    z[state] += state_range.seconds
+except FileNotFoundError:
+    print('Path Input Error, File Not Found')
+    quit()
+
+# Overflow
+#try:
+print('До:', data[fuserid])
+distribute_overflows(data[fuserid])
+#distribute_overflows(data[suserid])
+print('После:', data[fuserid])
+#except KeyError:
+    #print('Input Error, ID Or Date Not Found')
+    #quit()
+
+# Test bar chart
 xticks = np.arange(fcdate, lcdate, timedelta(hours=1)).astype(datetime)
 subsampled_xticks = map(lambda t: t.strftime("%m-%d"), xticks[0::24])
 xvals = np.arange(len(xticks))
 plt.xticks(xvals[0::24], subsampled_xticks)
-
-plt.ylabel("Минуты")
 plt.yticks(np.arange(0, 70, 10))
 
-fuser_busy_values = [
-    data[fuserid].get(time_group(time), {}).get('Busy', 0) / 60
-    for time in xticks
-]
-fuser_ready_values = [
-    data[fuserid].get(time_group(time), {}).get('Ready', 0) / 60
-    for time in xticks
-]
-fuser_rest_values = [
-    data[fuserid].get(time_group(time), {}).get('Rest', 0) / 60
-    for time in xticks
-]
-fuser_loggedout_values = [
-    data[fuserid].get(time_group(time), {}).get('LoggedOut', 0) / 60
-    for time in xticks
-]
-fuser_na_values = [
-    data[fuserid].get(time_group(time), {}).get('NA', 0) / 60
-    for time in xticks
-]
-fuser_servicebreak_values = [
-    data[fuserid].get(time_group(time), {}).get('ServiceBreak', 0) / 60
-    for time in xticks
-]
-fuser_dinner_values = [
-    data[fuserid].get(time_group(time), {}).get('Dinner', 0) / 60
-    for time in xticks
-]
+# Legend
+bars = [('Busy', 'Занят'), ('Ready', 'Готов'), ('Rest', 'Отдых'),
+        ('LoggedOut', 'Вышел'), ('Dinner', 'Обед'),
+        ('ServiceBreak', 'Тех. перерыв'), ('NA', '-')]
 
-busy = plt.bar(xvals, fuser_busy_values, 0.9)
-ready = plt.bar(xvals, fuser_ready_values, 0.9, bottom=fuser_busy_values)
-rest = plt.bar(xvals, fuser_rest_values, 0.9, bottom=fuser_ready_values)
-loggedout = plt.bar(xvals, fuser_loggedout_values, 0.9, bottom=fuser_rest_values)
-dinner = plt.bar(xvals, fuser_dinner_values, 0.9, bottom=fuser_loggedout_values)
-servicebreak = plt.bar(xvals, fuser_servicebreak_values, 0.9, bottom=fuser_dinner_values)
-na = plt.bar(xvals, fuser_na_values, 0.9, bottom=fuser_servicebreak_values)
-plt.legend((busy[0], ready[0], rest[0], loggedout[0], dinner[0], servicebreak[0], na[0]),
-           ('Занят', 'Готов', 'Отдых', 'Вышел', 'Обед', 'тех.перерыв', '-'))
+legend_bars = []
+prev_values = 0
 
+for (state, label) in bars:
+    values = [
+        data[fuserid].get(group_time(time), {}).get(state, 0) / 60
+        for time in xticks
+    ]
+    bar = plt.bar(xvals, values, 0.8, bottom=prev_values)
+    legend_bars.append(bar[0])
+    prev_values += np.array(values)
+
+plt.legend(legend_bars, map(lambda x: x[1], bars))
+
+# Multiple bar charts
+'''fig, (ax1, ax2) = plt.subplots(2)
+
+ax1.bar(xticks, values, 0.35)
+ax1.set_yticks(np.arange(0, 61, 10))
+ax1.set_title('fuser')
+ax1.set_ylabel('Минуты')
+
+ax2.bar(xticks, values, 0.35)
+ax2.set_yticks(np.arange(0, 61, 10))
+ax2.set_title('fuser2')
+ax2.set_ylabel('Минуты')
+
+fig.legend(legend_bars, map(lambda x: x[1], bars))'''
+
+# Output
 plt.show()
